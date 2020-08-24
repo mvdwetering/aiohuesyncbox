e42de56fb9bf016510ba2ea2b7b8a2896dc7a33c
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2020-08-24 18:31:06 +0200

    Extend set_state

--- aiohuesyncbox/execution.py
+++ aiohuesyncbox/execution.py
@@ -127,19 +127,25 @@ class Execution:
         data = {'intensity': intensity}
         await self._put(data)
 
-    async def set_state(self, sync_active=None, hdmi_active=None, mode=None, hdmi_source=None, brightness=None, video=None, game=None, music=None, intensity=None):
+    async def set_state(self, sync_active=None, sync_toggle=None, hdmi_active=None, hdmi_active_toggle=None, mode=None, mode_cycle=None, hdmi_source=None, hdmi_source_cycle=None, brightness=None, brightness_step=None, video=None, game=None, music=None, intensity=None, intensity_cycle=None):
         """Change execution state of huesyncbox."""
         data = {
             key: value for key, value in {
                 'syncActive': sync_active,
+                'toggleSyncActive': True if sync_toggle is True else None,
                 'hdmiActive': hdmi_active,
+                'toggleHdmiActive': True if hdmi_active_toggle is True else None,
                 'mode': mode,
+                'cycleSyncMode': mode_cycle,
                 'hdmiSource': hdmi_source,
+                'cycleHdmiSource': hdmi_source_cycle,
                 'brightness': brightness,
+                'incrementBrightness': brightness_step,
+                'intensity': intensity,
+                'cycleIntensity': intensity_cycle,
                 'video': video,
                 'game': game,
                 'music': music,
-                'intensity': intensity,
             }.items() if value is not None
         }
         await self._put(data)

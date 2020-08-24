3fd166e973e2ac5238e19a20de45017aec98acc4
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2020-08-24 18:31:58 +0200

    Bump to 0.0.15 and Production/Stable

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.14',
+    version='0.0.15',
 
     description='Asyncio package to communicate with a Philips Hue Play HDMI Sync Box.',
     long_description=long_description,
@@ -44,7 +44,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',

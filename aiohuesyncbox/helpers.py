296afa86056b397ac02b4e20e4f3d1efe171bcef
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2023-07-28 21:10:21 +0200

    Provide default when building attribute string

--- aiohuesyncbox/helpers.py
+++ aiohuesyncbox/helpers.py
@@ -6,5 +6,5 @@ from typing import List
 def generate_attribute_string(self, attributes: List) -> str:
     output = ""
     for attribute in attributes:
-        output += f"{attribute}: {getattr(self, attribute)}\n"
+        output += f"{attribute}: {getattr(self, attribute, None)}\n"
     return output

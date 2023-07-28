2b6434286c17a0be1b1f8ea7d64654ca7eebe506
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2023-07-28 21:09:26 +0200

    Export variables so pywright/pylance also understands it

--- aiohuesyncbox/__init__.py
+++ aiohuesyncbox/__init__.py
@@ -1,2 +1,6 @@
-from .errors import *
-from .huesyncbox import HueSyncBox
+from .errors import AiohuesyncboxException as AiohuesyncboxException
+from .errors import RequestError as RequestError
+from .errors import Unauthorized as Unauthorized
+from .errors import InvalidState as InvalidState
+
+from .huesyncbox import HueSyncBox as HueSyncBox

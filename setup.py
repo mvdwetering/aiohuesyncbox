8b0cb370bb44e53a39a9063d4ea1cf03e44da0ab
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2023-08-12 19:29:01 +0200

    Bump version to 0.0.26

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.0.25",
+    version="0.0.26",
     description="Asyncio package to communicate with a Philips Hue Play HDMI Sync Box.",
     long_description=long_description,
     # The project's main homepage.

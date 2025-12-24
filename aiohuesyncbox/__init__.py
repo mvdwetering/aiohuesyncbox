2820be55b123996b43a5e52286dd7e3569b22225
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2025-12-24 17:27:03 +0100

    Export more types

--- aiohuesyncbox/__init__.py
+++ aiohuesyncbox/__init__.py
@@ -4,6 +4,17 @@ from .errors import Unauthorized as Unauthorized
 from .errors import InvalidState as InvalidState
 
 from .huesyncbox import HueSyncBox as HueSyncBox
+from .hue import Group as Group
+from .hue import Hue as Hue
+from .behavior import Behavior as Behavior
+from .device import Device as Device
+from .device import Wifi as Wifi
+from .execution import Execution as Execution
+from .execution import SyncMode as SyncMode
+from .hdmi import Hdmi as Hdmi
+from .hdmi import Input as Input
+from .hdmi import Output as Output
+
 
 __all__ = [
     "AiohuesyncboxException",
@@ -11,4 +22,14 @@ __all__ = [
     "Unauthorized",
     "InvalidState",
     "HueSyncBox",
+    "Group",
+    "Hue",
+    "Behavior",
+    "Device",
+    "Wifi",
+    "Execution",
+    "SyncMode",
+    "Hdmi",
+    "Input",
+    "Output",
 ]
